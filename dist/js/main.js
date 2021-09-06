e18537f890958ad3e3a74e330c22663d2c49df13
Wesley Benites da Silva <wesleybenitesdasilva@gmail.com>
2021-09-06 08:52:39 -0400

    inicio

---
+++ dist/js/main.js
@@ -0,0 +1,9 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).ready(function () {
+    var banner = document.querySelector('[alt="www.000webhost.com"]').parentNode.parentNode;
+    banner.parentNode.removeChild(banner);
+});
